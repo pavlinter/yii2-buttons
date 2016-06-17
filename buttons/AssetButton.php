b083667cb675be41666413e80d223fbf08951187
Pavel <pavlinter@gmail.com>
2016-06-17 12:37:54 +0300

    @version 1.0.3

--- buttons/AssetButton.php
+++ buttons/AssetButton.php
@@ -4,7 +4,7 @@
  * @package yii2-buttons
  * @author Pavels Radajevs <pavlinter@gmail.com>
  * @copyright Copyright &copy; Pavels Radajevs <pavlinter@gmail.com>, 2015
- * @version 1.0.2
+ * @version 1.0.3
  */
 
 namespace pavlinter\buttons;
